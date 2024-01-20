510b68d3800855e22007a847ce5a01b6d153d128
Mike Ballou <mike@mikeballou.com>
2024-01-20 09:31:27 -0800

    Refactored all your base

--- python/all-your-base/all_your_base.py
+++ python/all-your-base/all_your_base.py
@@ -1,4 +1,4 @@
-def rebase(input_base, digits, output_base):
+def rebase(input_base: int, digits: list[int], output_base: int) -> list[int]:
     if input_base < 2:
         raise ValueError("input base must be >= 2")
     if output_base < 2:
@@ -6,19 +6,21 @@ def rebase(input_base, digits, output_base):
     return to_output_base(to_base_10(input_base, digits), output_base)
 
 
-def to_base_10(input_base, digits):
-    base_10 = 0
-    digits.reverse()
-    for index, digit in enumerate(digits):
+def to_base_10(input_base: int, digits: list[int]) -> int:
+    if not digits:
+        return 0
+    base_10 = digits[0]
+    for digit in digits[1:]:
         if not (0 <= digit < input_base):
             raise ValueError("all digits must satisfy 0 <= d < input base")
-        base_10 += digit * input_base**index
+        base_10 = base_10 * input_base + digit
     return base_10
 
 
-def to_output_base(base_10, output_base):
+def to_output_base(base_10: int, output_base: int) -> list[int]:
     output = []
     while base_10 > 0:
-        output.insert(0, base_10 % output_base)
-        base_10 //= output_base
-    return output if output else [0]
+        (base_10, rem) = divmod(base_10, output_base)
+        output.append(rem)
+    output.reverse()
+    return output or [0]
